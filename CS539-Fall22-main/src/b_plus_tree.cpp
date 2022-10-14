b6ac9c9a487b454cd97114e018e2a6f48d4395cd
SanchayKanade <sanchay2598@gmail.com>
2022-10-14 06:33:41 +0530

    Initial file load. All files are empty.

---
+++ CS539-Fall22-main/src/b_plus_tree.cpp
@@ -0,0 +1,50 @@
+#include "include/b_plus_tree.h"
+
+/*
+ * Helper function to decide whether current b+tree is empty
+ */
+bool BPlusTree::IsEmpty() const { return true; }
+
+/*****************************************************************************
+ * SEARCH
+ *****************************************************************************/
+/*
+ * Return the only value that associated with input key
+ * This method is used for point query
+ * @return : true means key exists
+ */
+bool BPlusTree::GetValue(const KeyType &key, RecordPointer &result) { return false; }
+
+/*****************************************************************************
+ * INSERTION
+ *****************************************************************************/
+/*
+ * Insert constant key & value pair into b+ tree
+ * If current tree is empty, start new tree, otherwise insert into leaf Node.
+ * @return: since we only support unique key, if user try to insert duplicate
+ * keys return false, otherwise return true.
+ */
+bool BPlusTree::Insert(const KeyType &key, const RecordPointer &value) { return false; }
+
+/*****************************************************************************
+ * REMOVE
+ *****************************************************************************/
+/*
+ * Delete key & value pair associated with input key
+ * If current tree is empty, return immdiately.
+ * If not, User needs to first find the right leaf node as deletion target, then
+ * delete entry from leaf node. Remember to deal with redistribute or merge if
+ * necessary.
+ */
+void BPlusTree::Remove(const KeyType &key) {}
+
+/*****************************************************************************
+ * RANGE_SCAN
+ *****************************************************************************/
+/*
+ * Return the values that within the given key range
+ * First find the node large or equal to the key_start, then traverse the leaf
+ * nodes until meet the key_end position, fetch all the records.
+ */
+void BPlusTree::RangeScan(const KeyType &key_start, const KeyType &key_end,
+                          std::vector<RecordPointer> &result) {}
